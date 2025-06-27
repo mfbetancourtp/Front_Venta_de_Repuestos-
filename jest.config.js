65dc52a28a6601b2f31f111c3f5c7563f952e81e
mfbetancourtp <42900005+mfbetancourtp@users.noreply.github.com>
2025-06-26 20:31:54 -0500

    Con una version compatible

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
 module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    transform: {
-        '^.+\\.(ts|html)$': ['ts-jest', {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-            stringifyContentPathRegex: '\\.html$',
-        }]
-    },
     testEnvironment: 'jsdom',
     moduleFileExtensions: ['ts', 'html', 'js', 'json'],
     coverageDirectory: 'coverage',
     collectCoverage: true,
-    collectCoverageFrom: ['src/app/**/*.ts', '!src/app/**/*.spec.ts'],
-    coverageReporters: ['lcov', 'text']
+    collectCoverageFrom: ['src/app/**/*.ts', '!src/app/**/*.spec.ts']
 };
